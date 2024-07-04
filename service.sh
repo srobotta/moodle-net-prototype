65308c7e1bb1a08c7e842f62f58c92f666d77ca2
Stephan Robotta <stephan.robotta@bfh.ch>
2024-07-04 16:49:59 +0200

    Allow restarting the service when start is used

--- service.sh
+++ service.sh
@@ -10,36 +10,69 @@ LOG_PREFIX=/home/ubuntu/nohup.out
 
 usage="Usage: $(basename $0) start|stop"
 
+function get_process_count() {
+  return $(ps aux | grep node | grep start.mjs | wc -l)
+}
+
+function stop_mnet() {
+  retry=0
+  get_process_count
+  if [ $? -gt 1 ]; then
+    retry=3
+  fi
+  while [ $retry -gt 0 ]; do
+    killall node
+    sleep 1
+    get_process_count
+    if [ $? -lt 2 ]; then
+      retry=0
+    else
+      retry=$((retry-1))
+    fi
+  done
+  get_process_count
+  if [ $? -gt 1 ]; then
+    echo "Could not stop MoodleNet"
+    exit 1
+  fi
+}
+
+function start_mnet() {
+  release_number=$(cat release.version)
+  if [ $? -ne 0 ]; then
+    echo "Warning: unknown release number"
+    release_number=1
+  fi
+  cd $INSTALL_DIR
+  nohup npm run dev-start-backend $DEV_NAME > ${LOG_PREFIX}.${release_number} 2>&1 &
+  sleep 2
+  get_process_count
+  if [ $? -gt 1 ]; then
+    echo "MoodleNet started"
+    exit 0
+  fi
+  echo "MoodleNet could not be started"
+  exit 1
+}
+
 if [ -z $1 ]; then
   echo $usage
   exit 1
 fi
 if [ "$1 " == "stop " ]; then
   echo "Stop MoodleNet"
-  killall node
+  stop_mnet
   exit 0
 fi
 if [ "$1 " == "start " ]; then
-  c=$(ps aux | grep node | grep start.mjs | wc -l)
-  if [ $c -gt 1 ]; then
-    echo "MoodleNet seems to be running"
-    exit 1
+  get_process_count
+  if [ $? -gt 1 ]; then
+    echo "MoodleNet seems to be running - restart"
+    stop_mnet
   fi
-  release_number=$(cat release.version)
-  if [ $? -ne 0 ]; then
-    echo "Warning: unknown release number"
-    release_number=XX
-  fi
-  cd $INSTALL_DIR
-  nohup npm run dev-start-backend $DEV_NAME > ${LOG_PREFIX}.${release_number} 2>&1 &
-  sleep 2
-  c=$(ps aux | grep node | grep start.mjs | wc -l)
-  if [ $c -gt 1 ]; then
-    echo "MoodleNet started"
-    exit 0
-  fi
-  echo "MoodleNet could not be started"
-  exit 1
+  start_mnet
 fi 
+
 echo $usage
 exit 1
+
